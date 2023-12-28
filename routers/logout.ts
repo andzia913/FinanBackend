ffd988067c620760498d5308f650e317417ffae0
Angelika Szczerbowska <andzia913@gmail.com>
2023-12-28 12:05:39 +0100

    Deleted useless router.

--- routers/logout.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-
-export const logoutRouter = Router();
-
-logoutRouter.post('/', (req, res) => {
-    res.clearCookie('token'); 
-    res.status(200).json({ success: true });
-  });
